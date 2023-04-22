7f3ff6b41d5c6b2d93b373daeb7358926b456eb8
Daemon0x00000000 <kd.daemon.kd@gmail.com>
2023-04-22 13:44:42 +0200

    reportdetail: Add of export to CSV function, button to save as .csv

--- report/reportdetail.js
+++ report/reportdetail.js
@@ -2,7 +2,7 @@ const params = new Proxy(new URLSearchParams(window.location.search), {
   get: (searchParams, prop) => searchParams.get(prop),
 });
 
-let strategyID = params.strategyID; 
+let strategyID = params.strategyID;
 
 chrome.storage.local.get("report-data-" + strategyID, function (item) {
   var reportDetailData = []
@@ -30,11 +30,43 @@ chrome.storage.local.get("report-data-" + strategyID, function (item) {
 
   setTimeout(() => {
     $table.bootstrapTable('load', reportDetailData)
-    $table.bootstrapTable('hideLoading')  
+    $table.bootstrapTable('hideLoading')
   }, 250);
-  
 
+  const $button = $('<button type="button" class="btn btn-primary btn-sm" id="download-report">Download Report</button>')
+  $button.click(function () {
+    saveCSVFile(reportDetailData)
+  })
+  $('.fixed-table-toolbar > .columns-right').append($button);
 });
+
+function convertToCSV(objArray) {
+    const keys = Object.keys(objArray[0]);
+    var result = keys.map((key) => {
+        return key.toUpperCase();
+    }).join(",") + "\r\n";
+
+    for (var i = 0; i < objArray.length; i++) {
+        var line = [];
+        for (var j = 0; j < keys.length; j++) {
+            var value = objArray[i][keys[j]];
+            if (typeof value === 'string' && value.indexOf(',') !== -1) {
+                value = '"' + value + '"';
+            }
+            line.push(value);
+        }
+        result += line.join(",") + "\r\n";
+    }
+    return result;
+}
+
+function saveCSVFile(data) {
+    const csv = convertToCSV(data)
+    const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+    const url = URL.createObjectURL(blob);
+    const link = $(`<a href="${url}" download="report-${strategyID}.csv"></a>`)
+    link[0].click();
+}
 
 // CustomSort function to handle non numeric chars and dash/hyphen confusion
 function customSort(sortName, sortOrder, data) {
@@ -49,14 +81,14 @@ function customSort(sortName, sortOrder, data) {
       }else{
         aa = +((a[sortName] + '').replace(/[^0-9.-]+/g,""))
       }
-      
+
       if(b[sortName].charAt(0).match(/\D/) != null ){
         bb = '-' + b[sortName].substring(1, b[sortName].length)
         bb = +((bb + '').replace(/[^0-9.-]+/g,""))
       }else{
         bb = +((b[sortName] + '').replace(/[^0-9.-]+/g,""))
       }
-      
+
       if (aa < bb) {
           return order * -1
       }
