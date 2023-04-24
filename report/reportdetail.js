f51d048b88cdc526147a0219233e4d7b8d7937c3
atakan.pehlivanoglu <pehlivanoglum@mef.edu.tr>
2023-04-25 02:17:38 +0300

    refactor csv feature

--- report/reportdetail.js
+++ report/reportdetail.js
@@ -8,10 +8,10 @@ chrome.storage.local.get("report-data-" + strategyID, function (item) {
   var reportDetailData = []
   var values = Object.values(item)[0].reportData
 
-  for (const [key, value] of Object.entries(values)){
+  for (const [key, value] of Object.entries(values)) {
     var reportDetail = {
-      "parameters" : key,
-      "netProfitAmount" : value.netProfit.amount,
+      "parameters": key,
+      "netProfitAmount": value.netProfit.amount,
       "netProfitPercent": value.netProfit.percent,
       "closedTrades": value.closedTrades,
       "percentProfitable": value.percentProfitable,
@@ -33,68 +33,74 @@ chrome.storage.local.get("report-data-" + strategyID, function (item) {
     $table.bootstrapTable('hideLoading')
   }, 250);
 
-  const $button = $('<button type="button" class="btn btn-primary btn-sm" id="download-report">Download Report</button>')
-  $button.click(function () {
-    saveCSVFile(reportDetailData)
+
+  const $downloadReportButton = $('#download-report')
+
+  $downloadReportButton.click(function () {
+    downloadCSVReport(reportDetailData)
   })
-  $('.fixed-table-toolbar > .columns-right').append($button);
 });
 
-function convertToCSV(objArray) {
-    const keys = Object.keys(objArray[0]);
-    var result = keys.map((key) => {
-        return key.toUpperCase();
-    }).join(",") + "\r\n";
+function downloadCSVReport(reportDetailData) {
+  const csv = convertReportToCSV(reportDetailData)
+  const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
+  const url = URL.createObjectURL(blob);
 
-    for (var i = 0; i < objArray.length; i++) {
-        var line = [];
-        for (var j = 0; j < keys.length; j++) {
-            var value = objArray[i][keys[j]];
-            if (typeof value === 'string' && value.indexOf(',') !== -1) {
-                value = '"' + value + '"';
-            }
-            line.push(value);
-        }
-        result += line.join(",") + "\r\n";
-    }
-    return result;
+  const link = document.createElement('a')
+  link.setAttribute('href', url)
+  link.setAttribute('download', `report-${strategyID}.csv`);
+
+  link.click();
 }
 
-function saveCSVFile(data) {
-    const csv = convertToCSV(data)
-    const blob = new Blob([csv], { type: "text/csv;charset=utf-8" });
-    const url = URL.createObjectURL(blob);
-    const link = $(`<a href="${url}" download="report-${strategyID}.csv"></a>`)
-    link[0].click();
+function convertReportToCSV(reportDetailData) {
+  const keys = Object.keys(reportDetailData[0]);
+  var result = keys.map((key) => {
+    return key.toUpperCase();
+  }).join(",") + "\n";
+
+  for (var i = 0; i < reportDetailData.length; i++) {
+    var line = [];
+    for (var j = 0; j < keys.length; j++) {
+      var value = reportDetailData[i][keys[j]];
+      // Enclose the value with "" if contains comma, to preserve format
+      if (typeof value === 'string' && value.indexOf(',') !== -1) {
+        value = '"' + value + '"';
+      }
+      line.push(value);
+    }
+    result += line.join(",") + "\n";
+  }
+  return result;
 }
 
 // CustomSort function to handle non numeric chars and dash/hyphen confusion
 function customSort(sortName, sortOrder, data) {
   var order = sortOrder === 'desc' ? -1 : 1
   data.sort(function (a, b) {
-      var aa = ""
-      var bb = ""
-      // Check if number is negative with regex, rebuild and remove non-numeric chars
-      if(a[sortName].charAt(0).match(/\D/) != null ){
-        aa = '-' + a[sortName].substring(1, a[sortName].length)
-        aa = +((aa + '').replace(/[^0-9.-]+/g,""))
-      }else{
-        aa = +((a[sortName] + '').replace(/[^0-9.-]+/g,""))
-      }
+    var aa = ""
+    var bb = ""
+    // Check if number is negative with regex, rebuild and remove non-numeric chars
+    if (a[sortName].charAt(0).match(/\D/) != null) {
+      aa = '-' + a[sortName].substring(1, a[sortName].length)
+      aa = +((aa + '').replace(/[^0-9.-]+/g, ""))
+    } else {
+      aa = +((a[sortName] + '').replace(/[^0-9.-]+/g, ""))
+    }
 
-      if(b[sortName].charAt(0).match(/\D/) != null ){
-        bb = '-' + b[sortName].substring(1, b[sortName].length)
-        bb = +((bb + '').replace(/[^0-9.-]+/g,""))
-      }else{
-        bb = +((b[sortName] + '').replace(/[^0-9.-]+/g,""))
-      }
+    if (b[sortName].charAt(0).match(/\D/) != null) {
+      bb = '-' + b[sortName].substring(1, b[sortName].length)
+      bb = +((bb + '').replace(/[^0-9.-]+/g, ""))
+    } else {
+      bb = +((b[sortName] + '').replace(/[^0-9.-]+/g, ""))
+    }
 
-      if (aa < bb) {
-          return order * -1
-      }
-      if (aa > bb) {
-          return order
-      }
-      return 0
+    if (aa < bb) {
+      return order * -1
+    }
+    if (aa > bb) {
+      return order
+    }
+    return 0
   })
 }
