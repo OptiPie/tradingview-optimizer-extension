9ad4db996fc21bbfe8f14b9e53b37d05fd8ab372
AtakanPehlivanoglu <pehlivanoglum@mef.edu.tr>
2024-02-07 02:26:34 +0300

    with background listeners

--- background.js
+++ background.js
@@ -3,16 +3,16 @@ chrome.runtime.onMessage.addListener((message, sender, reply) => {
   var properties = Object.keys(message)
   var values = Object.values(message)
   // Notify type represents chrome notification request
-  if(properties[0] === 'notify'){
+  if (properties[0] === 'notify') {
     var notification = values[0]
-    if(notification.type === 'warning'){
+    if (notification.type === 'warning') {
       chrome.notifications.create(`notify-${Date.now()}`, {
         title: 'OptiPie - Warning',
         message: notification.content,
         iconUrl: 'images/warning30.png',
         type: 'basic'
       });
-    }else if(notification.type === 'success'){
+    } else if (notification.type === 'success') {
       chrome.notifications.create(`notify-${Date.now()}`, {
         title: 'OptiPie - Success',
         message: notification.content,
@@ -23,9 +23,28 @@ chrome.runtime.onMessage.addListener((message, sender, reply) => {
   }
 });
 
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.type === "getAuthToken") {
+    chrome.identity.getAuthToken({ interactive: request.isInteractive }, function (token) {
+      sendResponse({ token: token });
+    });
+  }
+  return true
+});
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.type === "clearAllCachedAuthTokens") {
+      chrome.identity.getAuthToken({ interactive: false }, function (token) {
+        chrome.identity.removeCachedAuthToken({ token: token }, function () {});
+        chrome.identity.clearAllCachedAuthTokens();
+      });
+  }
+  return true
+});
+
 
 chrome.runtime.onStartup.addListener(() => {
   chrome.storage.local.set({ "userParameterCount": 1, "inputStart0": null, "inputEnd0": null, "inputStep0": null }, function () {
-    console.log("User parameter count state set to 0 at start up"); 
-  });  
+    console.log("User parameter count state set to 0 at start up");
+  });
 })
